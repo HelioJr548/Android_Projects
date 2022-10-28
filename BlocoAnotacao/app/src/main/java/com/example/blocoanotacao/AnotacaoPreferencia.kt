ac175388c23b41337283f36ff9ead45ab2c22ba7
HelioJr548 <heliojr548@gmail.com>
2022-10-27 21:14:02 -0300

    Arquivo para utilização do SharedPreferences

---
+++ BlocoAnotacao/app/src/main/java/com/example/blocoanotacao/AnotacaoPreferencia.kt
@@ -0,0 +1,27 @@
+package com.example.blocoanotacao
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
+
+class AnotacaoPreferencias(context: Context) {
+    private val preferences: SharedPreferences
+    private val NOME_ARQUIVO = "anotacao.preferencias"
+    private val editor: Editor
+    private val CHAVE_NOME = "nome"
+
+    fun salvarAnotacao(anotacao: String?) {
+        editor.putString(CHAVE_NOME, anotacao)
+        editor.commit()
+    }
+
+    fun recuperarAnotacao(): String? {
+        return preferences.getString(CHAVE_NOME, "")
+    }
+
+    init {
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, 0)
+        editor = preferences.edit()
+    }
+
+}
