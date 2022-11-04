be056237cc604da2c74c9fa9df7622be405caa93
HelioJr548 <heliojr548@gmail.com>
2022-11-04 18:47:22 -0300

    Criação de tabela

--- SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
+++ SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.sqlite
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +12,12 @@ class MainActivity : AppCompatActivity() {
 //            CRIAR DB
             val bd = openOrCreateDatabase("copa", MODE_PRIVATE, null)
 
-
+//            CRIAR TABELA COM AUTOINCREMENT
+            bd.execSQL(
+                "CREATE TABLE IF NOT EXISTS jogosCopa " +
+                        "(jogo INTEGER PRIMARY KEY AUTOINCREMENT, info VARCHAR, " +
+                        "data VARCHAR, selecao VARCHAR, rival VARCHAR, placar VARCHAR)"
+            )
         } catch (e: Exception) {
             e.printStackTrace()
         }
