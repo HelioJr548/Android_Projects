5af2bdd9f8c13f860f5ef548a0ac525a8cd3ebe4
HelioJr548 <heliojr548@gmail.com>
2022-11-04 18:49:19 -0300

    Insert de dados

--- SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
+++ SQLite/app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -18,6 +18,61 @@ class MainActivity : AppCompatActivity() {
                         "(jogo INTEGER PRIMARY KEY AUTOINCREMENT, info VARCHAR, " +
                         "data VARCHAR, selecao VARCHAR, rival VARCHAR, placar VARCHAR)"
             )
+
+//            INSERT DE DADOS
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Amistoso', '23/09/2022', 'Brasil', 'Gana', '3x0')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Amistoso', '27/09/2022', 'Brasil', 'Tunísia', '5x1')"
+            )
+
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Cerimonia de Abertura da Copa', '20/11/2022', 'Qatar', 'Equador', '2x1')"
+            )
+
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Fase de Grupos', '24/11/2022', 'Brasil', 'Sérvia', '4x0')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Fase de Grupos', '28/11/2022', 'Brasil', 'Suíça', '3x1')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Fase de Grupos', '02/12/2022', 'Brasil', 'Camarões', '5x0')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Oitavas de final', '06/12/2022', 'Brasil', 'Portugal', '2x0')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Quartas de final', '10/12/2022', 'Brasil', 'Alemanha', '2x1')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Semifinais', '14/12/2022', 'Brasil', 'Argentina', '3x1')"
+            )
+
+            bd.execSQL(
+                "INSERT INTO jogosCopa (info, data, selecao, rival, placar) " +
+                        "VALUES ('Final', '18/12/2022', 'Brasil', 'Inglaterra', '3x1')"
+            )
+
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
