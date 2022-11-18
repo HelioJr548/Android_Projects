e580fda352882378b56a1f966b7dbe9da505409b
HelioJr548 <heliojr548@gmail.com>
2022-11-18 17:19:33 -0300

    Código em java

--- Java_Kotlin/app/src/main/java/com/example/java_kotlin/MainActivity.java
+++ Java_Kotlin/app/src/main/java/com/example/java_kotlin/MainActivity.java
@@ -1,56 +1,63 @@
-package com.example.java_kotlin
+package com.example.java_kotlin;
 
-import android.support.v7.app.AppCompatActivity
-import android.widget.EditText
-import android.widget.TextView
-import android.os.Bundle
-import com.example.java_kotlin.R
-import android.content.SharedPreferences
-import android.view.View
-import android.widget.Button
-import com.example.java_kotlin.MainActivity
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-class MainActivity : AppCompatActivity() {
-//    private lateinit var btnSalvar: Button? = null
-//    private lateinit var txtNome: EditText? = null
-//    private lateinit var lblResultado: TextView? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var txtNome = findViewById<EditText>(R.id.txtNome)
-        var btnSalvar = findViewById<Button>(R.id.btnSalvar)
-        var lblResultado = findViewById<TextView>(R.id.lblResultado)
-        btnSalvar.setOnClickListener(View.OnClickListener {
-            val preferences = getSharedPreferences(ARQUIVO_REFENCIA, 0)
-            val editor = preferences.edit()
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSalvar;
+    private EditText txtNome;
+    private EditText txtSobNome;
+    private TextView lblResultado;
+    private static String ARQUIVO_REFENCIA = "ArquivoPreferencia";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNome = findViewById(R.id.txtNome);
+        txtSobNome = findViewById(R.id.txtSobNome);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        lblResultado = findViewById(R.id.lblResultado);
+
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences(ARQUIVO_REFENCIA, 0);
+                SharedPreferences.Editor editor = preferences.edit();
 
 //                VALIDAR NOME
-            if (txtNome.getText().toString() == "") {
-                Toast.makeText(
-                    applicationContext,
-                    "Por favor, preencher seu nome",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else {
-                val nome = txtNome.getText().toString()
-                editor.putString("nome", nome)
-                editor.commit()
-                lblResultado.setText("olá $nome")
+                if (txtNome.getText().toString().equals("") || txtSobNome.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Atenção, Nome ou Sobrenome vazios.\nPor favor preencha-o(s)", Toast.LENGTH_LONG).show();
+                } else {
+                    String nome = txtNome.getText().toString();
+                    editor.putString("nome", nome);
+                    String sobnome = txtSobNome.getText().toString();
+                    editor.putString("sobnome", sobnome);
+                    editor.commit();
+                    lblResultado.setText("Seja Bem-vindo(a)\n" + nome + " " + sobnome);
+                }
+
             }
-        })
+        });
 
 //        RECUPERAR DADOS SALVOS
-        val preferences = getSharedPreferences(ARQUIVO_REFENCIA, 0)
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_REFENCIA, 0);
         if (preferences.contains("nome")) {
-            val nome = preferences.getString("nome", "\" Olá, usuário não defenido")
-            lblResultado.setText("Olá $nome")
+            String nome = preferences.getString("nome", "\" Olá, usuário não defenido");
+            String sobnome = preferences.getString("sobnome", "\" Olá, usuário não defenido");
+            lblResultado.setText("Seja Bem-vindo(a)\n" + nome + " " + sobnome);
         } else {
-            lblResultado.setText("Olá, usuário não definido")
+            lblResultado.setText("Olá, usuário não definido");
         }
-    }
-
-    companion object {
-        private const val ARQUIVO_REFENCIA = "ArquivoPreferencia"
     }
 }
